9cd674af5932bb78b3a8dcfe58c6d7991b2fcfd0
HCarlos <r0@tecnointel.mx>
2018-02-16 07:55:02 -0600

    Lati - 1N Alfa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                   <strong>{{ Auth::user()->name }}</strong> You are logged in!
                 </div>
             </div>
         </div>
