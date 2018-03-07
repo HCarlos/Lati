243553de1d20f303d247c366f8e7acf88874c918
HCarlos <r0@tecnointel.mx>
2018-03-07 14:46:06 -0600

    Lati - 3Bg Alfa

--- resources/views/catalogos/side_bar_left.blade.php
+++ resources/views/catalogos/side_bar_left.blade.php
@@ -4,13 +4,15 @@
     </div>
 @endif
 <div class="list-group todoloAncho" id="dvCatalogos0">
+    @role('user|administrator')
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 0)) }}">Editoriales</a>
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 1)) }}">Fichas</a>
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 2)) }}">Códigos de lenguaje de paises</a>
     {{--<a></a>--}}
     {{--<a class="button list-group-item form-control" href="#" id="btnPrueba">Prueba</a>--}}
+    @endrole
 
-    @role('administrator|system_operator')
+    @role('administrator')
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 10)) }}">Usuarios</a>
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 11)) }}">Roles</a>
     <a class="button list-group-item form-control" href="{{ route('listItem', array('id' => 12)) }}">Permisos</a>
