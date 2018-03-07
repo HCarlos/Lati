e5d021f762cafe5cb24663266736d36dabb51eb8
HCarlos <r0@tecnointel.mx>
2018-03-07 14:50:33 -0600

    Lati - 3Bh Alfa

--- resources/views/catalogos/side_bl_config.blade.php
+++ resources/views/catalogos/side_bl_config.blade.php
@@ -1,4 +1,5 @@
     @role('administrator|system_operator')
+
     @if (session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
@@ -8,4 +9,5 @@
         <a class="button list-group-item form-control" href="{{ route('asignItem/', array('ida' => 0,'iduser' => 0)) }}">Asignar Roles a Usuario</a>
         <a class="button list-group-item form-control" href="{{ route('asignItem/', array('ida' => 1,'iduser' => 0)) }}">Asignar Permisos a Role</a>
     </div>
+
     @endrole
