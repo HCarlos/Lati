e4a9cd443ff46fc8147a916b05148efe2a58f59d
HCarlos <r0@tecnointel.mx>
2018-03-16 11:37:56 -0600

    Lati - 3Cd Alfa

--- app/Notifications/MyResetPassword.php
+++ app/Notifications/MyResetPassword.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
+use PharIo\Manifest\InvalidEmailException;
 
 class MyResetPassword extends ResetPassword
 {
@@ -14,14 +15,19 @@ class MyResetPassword extends ResetPassword
 //            ->line('You are receiving this email because we received a password reset request for your account.')
 //            ->action('Reset Password', route('password.reset', $this->token))
 //            ->line('If you did not request a password reset, no further action is required.');
+        try{
 
-        return (new MailMessage)
-            ->subject('Recuperar contraseña')
-            ->greeting('Hola')
-            ->line('Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.')
-            ->action('Recuperar contraseña', route('password.reset', $this->token))
-            ->line('Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.')
-            ->salutation('Saludos, '. config('app.name'));
+                return (new MailMessage)
+                    ->subject('Recuperar contraseña')
+                    ->greeting('Hola')
+                    ->line('Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.')
+                    ->action('Recuperar contraseña', route('password.reset', $this->token))
+                    ->line('Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.')
+                    ->salutation('Saludos, '. config('app.name'));
+
+        }catch (InvalidEmailException $e){
+            dd($e);
+        }
 
     }
 
